bed82f5f960bb2f5130e8bde07c565e83ca1ec48
Raul A Rozo <raul.a.rozo.o@gmail.com>
2020-01-16 02:53:51 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -47,7 +47,7 @@ def total_even_pairs(src)
 row_index = 0
 while row_index < src.count do
  if src[row_index][0] % 2 == 0 && src[row_index][1] % 2 == 0
-   total += total
+   total += src[row_index][0] + src[row_index][1]
   row_index += 1
 end
 total
